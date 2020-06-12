f79edde1276e15a3a573c842409a1d6dc004877a
zhouduanrui <805719724@qq.com>
2020-06-12 17:33:42 +0800

    1.现在新增了guava的布隆过滤器测试

--- springcloud-provider/src/main/java/com/dr/filter/BloomFilterUtil.java
+++ springcloud-provider/src/main/java/com/dr/filter/BloomFilterUtil.java
@@ -8,13 +8,44 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.UUID;
+
 @Aspect
 @Component
 public class BloomFilterUtil {
-    private final static int insertions = 10000000;
+    private final static int insertions = 1000000;
+    static HashSet<String> hashSet = new HashSet();
+    static ArrayList<String> arrayList = new ArrayList();
+    static BloomFilter bloomFilter;
     static {
-        BloomFilter bloomFilter = BloomFilter.create(Funnels.stringFunnel(Charsets.UTF_8), 1000);
+        bloomFilter = BloomFilter.create(Funnels.stringFunnel(Charsets.UTF_8), insertions);
 
+        for(int i=0;i<insertions;i++){
+            String uuid = UUID.randomUUID().toString();
+            bloomFilter.put(uuid);
+            hashSet.add(uuid);
+            arrayList.add(uuid);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int rigth = 0;
+        int wrong = 0;
+        for(int i=0; i<insertions; i++){
+            String test = i%100==0? arrayList.get(i):UUID.randomUUID().toString();
+            if(bloomFilter.mightContain(test)){
+                if(hashSet.contains(test)){
+                    rigth++;
+                }else {
+                    wrong++;
+                }
+            }
+        }
+        System.out.println("rigth="+ rigth);
+        System.out.println("wrong="+ wrong);
     }
 
     @Pointcut("")
